013695ce9c72c71b7e6bb2dccd65f5c5a2921eb5
adeniasfilho <adeniasfilho.n1@gmail.com>
2021-03-18 20:47:29 -0300

    Códigos adicionados

--- Empregado.java
+++ Empregado.java
@@ -42,4 +42,23 @@ public class Empregado {
     public void setIdade(int idade) {
         this.idade = idade;
     }
+//adicionar à classe Empregado.java
+public void setSalario (double salario){
+    this.salario = salario;
+    }
+    public double getSalario (){
+    return this.salario;
+    }
+    public void setComissao (double comissao){
+    this.comissao = comissao;
+    }
+    public double getComissao (){
+    return this.comissao;
+    }
+    public void setBonus (double bonus){
+    this.bonus = bonus;
+    }
+    public double getBonus (){
+    return this.bonus;
+    }
 }
