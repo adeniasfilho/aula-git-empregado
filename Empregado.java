7dd37064804f18415ce318e334ce2e94afdcb30f
adeniasfilho <adeniasfilho.n1@gmail.com>
2021-03-18 20:52:58 -0300

    Código salvo

--- Empregado.java
+++ Empregado.java
@@ -42,23 +42,29 @@ public class Empregado {
     public void setIdade(int idade) {
         this.idade = idade;
     }
-//adicionar à classe Empregado.java
-public void setSalario (double salario){
-    this.salario = salario;
+
+    // adicionar à classe Empregado.java
+    public void setSalario(double salario) {
+        this.salario = salario;
     }
-    public double getSalario (){
-    return this.salario;
+
+    public double getSalario() {
+        return this.salario;
     }
-    public void setComissao (double comissao){
-    this.comissao = comissao;
+
+    public void setComissao(double comissao) {
+        this.comissao = comissao;
     }
-    public double getComissao (){
-    return this.comissao;
+
+    public double getComissao() {
+        return this.comissao;
     }
-    public void setBonus (double bonus){
-    this.bonus = bonus;
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
     }
-    public double getBonus (){
-    return this.bonus;
+
+    public double getBonus() {
+        return this.bonus;
     }
 }
